85b1229e3de3df0c4dfd6399bb10f22c6acc2710
bertux-8 <150936150+bertux-8@users.noreply.github.com>
2024-03-06 10:52:18 +0100

    modify views site

--- mysite/fitness/views.py
+++ mysite/fitness/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the fitness app ")
+def fitness(request):
+    return HttpResponse("Hello, world. You're at the fitness site ")
 
