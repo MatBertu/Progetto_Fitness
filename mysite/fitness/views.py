ce214223a1a712e40092c9298952d2ba43d02dce
bertux-8 <150936150+bertux-8@users.noreply.github.com>
2024-03-21 12:12:15 +0100

    modifica vista

--- mysite/fitness/views.py
+++ mysite/fitness/views.py
@@ -6,6 +6,17 @@ from .models import Member
 def fitness(request):
   members = Member.objects.all().values()
   template = loader.get_template('templates.html')
+ 
+
+  context = {
+    'members': members
+  }
+  return HttpResponse(template.render(context, request))
+
+def allenamenti(request):
+  members = Member.objects.all().values()
+  template = loader.get_template('allenamenti.html')
+ 
 
   context = {
     'members': members
