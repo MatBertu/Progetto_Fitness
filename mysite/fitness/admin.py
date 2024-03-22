8c78505c6a0526055e2f68c6c3f2564d9a23a086
bertux-8 <150936150+bertux-8@users.noreply.github.com>
2024-03-22 10:05:57 +0100

    modify admin

--- mysite/fitness/admin.py
+++ mysite/fitness/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Member
+from .models import CaratteristicheFisiche
+from .models import  ObiettivoFitness
 
+admin.site.register( ObiettivoFitness)
+admin.site.register(CaratteristicheFisiche)
 admin.site.register(Member)
