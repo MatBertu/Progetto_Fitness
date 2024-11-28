6919d057995efccaf95dc08f76a3f62a6b0c7bd5
bertux-8 <150936150+bertux-8@users.noreply.github.com>
2024-11-28 19:02:46 +0100

    urls

--- mysite/fitness/urls.py
+++ mysite/fitness/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('fitness/', views.fitness, name='fitness'),
-    path('allenamenti/', views.allenamenti, name='allenamenti'),
-    path("ollama/",views.ollama, name = "ollama"),
     path("workout/",views.workout, name = " workout"),
-    path("index/", views.index, name = "index")
+    path("ollama/",views.ollama, name = "ollama"),
+    
+   
 ]
