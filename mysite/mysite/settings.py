50eebcf1e09e63ad8ee6571d598e4f67bf41f0d6
bertux-8 <150936150+bertux-8@users.noreply.github.com>
2025-01-10 10:43:08 +0100

    change redirection url after login

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-LOGIN_REDIRECT_URL = '/fitness/add_caratteristica_fisica'
+LOGIN_REDIRECT_URL = '/fitness/overview'
 
 STATIC_URL = '/static/'
 
